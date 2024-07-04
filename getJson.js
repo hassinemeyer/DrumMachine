556197627a434a97ed2823898735c28b4ad78b96
hassinemeyer <43935429+hassinemeyer@users.noreply.github.com>
2024-07-04 13:39:10 +0200

    Update getJson.js

--- getJson.js
+++ getJson.js
@@ -1,11 +1,13 @@
-export async function fetch(url) {
+export async function fetchJSON(url) {
   try {
+    console.log(`Fetching JSON data from ${url}`); // Log the URL being fetched
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Error fetching JSON data: ${response.status}`);  
     }
 
     const jsonData = await response.json();
+    console.log('JSON data successfully fetched:', jsonData); // Log the fetched data
     return jsonData;
   } catch (error) {
     console.error('Error:', error);
