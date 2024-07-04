f79a6e92c22efe9903e1a2f5b34155b82f273366
hassinemeyer <43935429+hassinemeyer@users.noreply.github.com>
2024-07-04 13:16:58 +0200

    Create getJson.js

---
+++ getJson.js
@@ -0,0 +1,14 @@
+async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error fetching JSON data: ${response.status}`);  
+    }
+
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.error('Error:', error);
+    return null;  
+  }
+}
